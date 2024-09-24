c467d14142bcf61a40e98a175dc0398e6edca53a
tsukumi <tsukumijima@users.noreply.github.com>
2024-09-25 01:23:03 +0900

    Update: パッチ適用スクリプトを改良

--- Patches/apply_patch.sh
+++ Patches/apply_patch.sh
@@ -4,13 +4,14 @@ P_OPT=
 [ -n "$MINGW_PREFIX" ] && P_OPT=--binary
 P_DIR="$(dirname "$0")"
 
-patch $P_OPT -p1 < "$P_DIR"/0001-*.patch || exit
-patch $P_OPT -p1 < "$P_DIR"/0002-*.patch || exit
-patch $P_OPT -p1 < "$P_DIR"/0003-*.patch || exit
-patch $P_OPT -p1 < "$P_DIR"/0004-*.patch || exit
-patch $P_OPT -p1 < "$P_DIR"/0005-*.patch || exit
-patch $P_OPT -p1 < "$P_DIR"/0006-*.patch || exit
-patch $P_OPT -p1 < "$P_DIR"/0007-*.patch || exit
-patch $P_OPT -p1 < "$P_DIR"/0008-*.patch || exit
-patch $P_OPT -p1 < "$P_DIR"/0009-*.patch || exit
-patch $P_OPT -p1 < "$P_DIR"/0010-*.patch || exit
+# Apply and commit each patch
+for patch in "$P_DIR"/00*.patch; do
+  # Apply the patch
+  patch $P_OPT -p1 < "$patch" || exit
+  # Extract commit message from patch file and remove "Subject: " prefix
+  commit_message=$(sed -n '4s/^Subject: //p' "$patch")
+  # Add changes to git
+  git add -A
+  # Commit changes with the extracted commit message
+  git commit -m "$commit_message"
+done
